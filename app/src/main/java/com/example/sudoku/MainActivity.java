aa4c3ccd32426da3ff1a8b991fff3e21c342cd24
Sieun Park <psieun0606@gmail.com>
2022-11-26 02:28:03 +0900

    feat: edit screen configuration

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.sudoku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class MainActivity extends AppCompatActivity {
+    TableLayout table;
+    private final int left = 3;
+    private final int top = 3;
+    private final int right = 3;
+    private final int bottom = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TableRow.LayoutParams layoutParams =
+                new TableRow.LayoutParams(
+                        TableRow.LayoutParams.WRAP_CONTENT,
+                        TableRow.LayoutParams.WRAP_CONTENT,
+                        1.0f);
+        layoutParams.setMargins(left, top, right, bottom);
+
+        Button[][] buttons = new Button[9][9];
+        for (int i = 0; i < 9; i++) {
+            table = (TableLayout)findViewById(R.id.tableLayout);
+            TableRow tableRow = new TableRow(this);
+            table.addView(tableRow);
+            for (int j = 0; j < 9; j++) {
+                buttons[i][j] = new Button(this);
+                buttons[i][j].setLayoutParams(layoutParams);
+                buttons[i][j].setBackgroundColor(Color.GRAY);
+                tableRow.addView(buttons[i][j]);
+            }
+        }
     }
 }
