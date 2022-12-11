a3a183cde86d8afba4dc5012d7dbbc53beee4899
Sieun Park <psieun0606@gmail.com>
2022-12-11 11:56:20 +0900

    feat: add set in CustomButton

--- app/src/main/java/com/example/sudoku/CustomButton.java
+++ app/src/main/java/com/example/sudoku/CustomButton.java
@@ -1,6 +1,7 @@
 package com.example.sudoku;
 
 import android.content.Context;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
@@ -18,5 +19,12 @@ public class CustomButton extends FrameLayout {
         addView(textView);
         setClickable(true);
         setBackgroundResource(R.drawable.button_selector);
+    }
+
+    public void set(int isDisplayNumber, String buttonText) {
+        if (isDisplayNumber == 0) {
+            textView.setVisibility(View.INVISIBLE);
+        }
+        textView.setText(buttonText);
     }
 }
